e51e7970e6c0dd704f2089ac2ca41c379c18b7d1
harukitosa <13haruki28@gmail.com>
2020-09-01 11:15:55 +0900

    Dev:テストデータの作成

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,8 +1,5 @@
 package a
 
-func f() {
-	// The pattern can be written in regular expression.
-	var gopher int // want "pattern"
-	print(gopher)  // want "identifier is gopher"
+func sample1() error {
+	return nil
 }
-
