b9503585d8a5541930b5b95aee585672a4c92be5
harukitosa <13haruki28@gmail.com>
2020-09-03 11:59:23 +0900

    Fix:こまかな修正

--- testdata/src/sample_test/sample_test1.go
+++ testdata/src/sample_test/sample_test1.go
@@ -1,5 +1,7 @@
 package sample_test1
 
+import "errors"
+
 func sample1() error { // want "It returns nil in all the places where it should return error"
 	if false {
 		s := 0
@@ -16,18 +18,18 @@ func sample1() error { // want "It returns nil in all the places where it should
 	return nil
 }
 
-// func sample2() error {
-// 	if false {
-// 		s := 0
-// 		s++
-// 		if s >= 1 {
-// 			s++
-// 		} else {
-// 			s++
-// 			for {
-// 				return errors.New("error")
-// 			}
-// 		}
-// 	}
-// 	return errors.New("error")
-// }
+func sample2() error {
+	if false {
+		s := 0
+		s++
+		if s >= 1 {
+			s++
+		} else {
+			s++
+			for {
+				return errors.New("error")
+			}
+		}
+	}
+	return errors.New("error")
+}
