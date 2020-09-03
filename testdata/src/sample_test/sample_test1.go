eb55d9799c9a96a41ca4830f5f8465d53b6fb49b
harukitosa <13haruki28@gmail.com>
2020-09-03 11:56:54 +0900

    Fix:README.mdの改定 #8

--- testdata/src/sample_test/sample_test1.go
+++ testdata/src/sample_test/sample_test1.go
@@ -1,9 +1,5 @@
 package sample_test1
 
-import (
-	"errors"
-)
-
 func sample1() error { // want "It returns nil in all the places where it should return error"
 	if false {
 		s := 0
@@ -20,18 +16,18 @@ func sample1() error { // want "It returns nil in all the places where it should
 	return nil
 }
 
-func sample2() error {
-	if false {
-		s := 0
-		s++
-		if s >= 1 {
-			s++
-		} else {
-			s++
-			for {
-				return errors.New("error")
-			}
-		}
-	}
-	return errors.New("error")
-}
+// func sample2() error {
+// 	if false {
+// 		s := 0
+// 		s++
+// 		if s >= 1 {
+// 			s++
+// 		} else {
+// 			s++
+// 			for {
+// 				return errors.New("error")
+// 			}
+// 		}
+// 	}
+// 	return errors.New("error")
+// }
